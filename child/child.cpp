a15e8f2028abe432f5df59acefcd8fd1fb7a848f
IsPorCHen <fazy2019@gmail.com>
2025-02-20 18:42:00 +0500

    Добавьте файлы проекта.

---
+++ child/child.cpp
@@ -0,0 +1,87 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#define PIPE_NAME L"\\\\.\\pipe\\MyPipe"
+
+vector<HANDLE> threads;
+
+DWORD WINAPI ThreadFunction(LPVOID lpParam) {
+    int id = *(int*)lpParam;
+    srand(GetTickCount() + id);
+    int a = rand() % 100;
+    int b = rand() % 100;
+    cout << "[Поток " << id << "] Сумма: " << a << " + " << b << " = " << (a + b) << endl;
+    return 0;
+}
+
+void CreateThreads(int count) {
+    for (int i = 0; i < count; i++) {
+        int* threadID = new int(i + 1);
+        HANDLE hThread = CreateThread(NULL, 0, ThreadFunction, threadID, 0, NULL);
+        if (hThread) {
+            cout << "[Дочерний процесс] Создан поток " << (*threadID) << endl;
+            threads.push_back(hThread);
+        }
+    }
+}
+
+void DeleteThread(int id) {
+    if (id > 0 && id <= threads.size()) {
+        TerminateThread(threads[id - 1], 0);
+        CloseHandle(threads[id - 1]);
+        threads.erase(threads.begin() + id - 1);
+        cout << "[Дочерний процесс] Поток " << id << " удален." << endl;
+    }
+}
+
+int main() {
+    setlocale(0, "rus");
+    HANDLE hPipe = CreateFile(PIPE_NAME, GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL);
+    if (hPipe == INVALID_HANDLE_VALUE) {
+        cerr << "[Дочерний процесс] Ошибка подключения к каналу! Код ошибки: " << GetLastError() << endl;
+        return 1;
+    }
+
+    cout << "[Дочерний процесс] Подключение к каналу успешно." << endl;
+
+    char buffer[256];
+    while (true) {
+        DWORD bytesRead;
+        if (ReadFile(hPipe, buffer, sizeof(buffer), &bytesRead, NULL)) {
+            string command(buffer);
+
+            if (!command.empty()) {
+                cout << "[Дочерний процесс] Получена команда: " << command << endl;
+
+                if (command[0] == '1') {
+                    int count = stoi(command.substr(2));
+                    CreateThreads(count);
+                }
+                else if (command[0] == '2') {
+                    int id = stoi(command.substr(2));
+                    DeleteThread(id);
+                }
+                else if (command[0] == '3') {
+                    break;
+                }
+            }
+        }
+
+        memset(buffer, 0, sizeof(buffer)); // Очищаем буфер после обработки
+        Sleep(500);
+    }
+
+    // Очистка
+    for (HANDLE thread : threads) {
+        TerminateThread(thread, 0);
+        CloseHandle(thread);
+    }
+
+    CloseHandle(hPipe);
+    cout << "[Дочерний процесс] Завершение работы." << endl;
+    return 0;
+}
---
+++ process13/process13.cpp
@@ -0,0 +1,59 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define PIPE_NAME L"\\\\.\\pipe\\MyPipe"
+
+int main() {
+    setlocale(0, "rus");
+
+    HANDLE hPipe = CreateNamedPipe(PIPE_NAME, PIPE_ACCESS_OUTBOUND, PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT, 1, 256, 256, 0, NULL);
+    if (hPipe == INVALID_HANDLE_VALUE) {
+        cerr << "[Родительский процесс] Ошибка создания канала! Код ошибки: " << GetLastError() << endl;
+        return 1;
+    }
+
+    cout << "[Родительский процесс] Канал создан, ожидаем подключения дочернего процесса..." << endl;
+
+    STARTUPINFO si = { sizeof(si) };
+    PROCESS_INFORMATION pi;
+    if (!CreateProcess(L"C:\\Users\\ipch\\Downloads\\process13-master\\process13-master\\x64\\Debug\\child.exe", NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, NULL, &si, &pi)) {
+        cerr << "[Родительский процесс] Ошибка создания дочернего процесса!" << GetLastError() <<endl;
+        CloseHandle(hPipe);
+        return 1;
+    }
+
+    if (!ConnectNamedPipe(hPipe, NULL)) {
+        cerr << "[Родительский процесс] Ошибка подключения к каналу! Код ошибки: " << GetLastError() << endl;
+        CloseHandle(hPipe);
+        return 1;
+    }
+
+    cout << "[Родительский процесс] Дочерний процесс подключен." << endl;
+
+    string command;
+    while (true) {
+        cout << "[Родительский процесс] Введите команду (1 <количество> - создание потоков\n2 <id> - удаление потока\n3 - выход): ";
+        getline(cin, command);
+
+        DWORD bytesWritten;
+        WriteFile(hPipe, command.c_str(), command.size() + 1, &bytesWritten, NULL);
+
+        if (command == "3") {
+            break;
+        }
+
+        Sleep(500);
+    }
+
+    CloseHandle(hPipe);
+    cout << "[Родительский процесс] Завершение работы." << endl;
+
+    WaitForSingleObject(pi.hProcess, INFINITE);
+    CloseHandle(pi.hProcess);
+    CloseHandle(pi.hThread);
+
+    return 0;
+}
